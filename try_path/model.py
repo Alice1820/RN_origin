d24ef7a0b16c7584ddb9d3721c1223d52f5cb2fd
Alice1820 <18001208205@163.com>
2017-07-21 11:12:54 +0800

    Add files via upload

---
+++ try_path/model.py
@@ -0,0 +1,386 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 16:28:50 2017
+
+@author: xifan
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
+import tensorflow.contrib.slim as slim
+import numpy as np
+import re
+from tensorflow.python.ops.nn_ops import max_pool
+from scipy.misc import imresize
+import tensorflow.contrib.slim as slim
+from ops import conv2d, fc
+
+#from ops import conv2d, fc
+
+#from train import GLOVE
+TOWER_NAME = 'tower'
+weight_file = 'vgg16_weights.npz'
+
+NUM_CLASSES = 50
+#batch_size = 128
+MX_LEN = 64 # the max length of a sentence
+EMBEDDING_DIM = 50 # word look-up embeddings dim
+
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 700000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+
+MOVING_AVERAGE_DECAY = 0.9999
+NUM_EPOCHS_PER_DECAY = 350
+LEARNING_RATE_DECAY_FACTOR = 0.5
+INITIAL_LEARNING_RATE = 2.5e-4
+
+QUESTION_DICT_LENGTH = 80
+ANSWER_DICT_LENGTH = 28
+
+conv_info = np.array([64, 64, 64, 64])
+
+def _activation_summary(x):
+    """
+    Helper to create summaries for activation
+    Creates a summary that provides a histogram of activation
+    Creates a summary that measure the sparsity of activaiton
+    """
+    
+    # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
+    # session. This helps the clarity of presentation on tensorboard.
+    tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+    
+    tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
+    tf.summary.histogram(tensor_name + '/activation', x)
+
+
+def _variable_on_cpu(name, shape, initializer):
+    """Helper to create a Variable stored on CPU memory.
+    Args:
+      name: name of the variable
+      shape: list of ints
+      initializer: initializer for Variable
+    Returns:
+      Variable Tensor
+    """
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer = initializer)
+        
+    return var
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+    """Helper to create an initialized Variable with weight decay.
+    Note that the Variable is initialized with a truncated normal distribution.
+    A weight decay is added only if one is specified.
+    """
+    var = _variable_on_cpu(name, shape, tf.truncated_normal_initializer(stddev = stddev))
+    if wd:
+        weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name = 'weight_loss')
+        tf.add_to_collection('losses', weight_decay)
+    
+    return var
+
+#def concat_coor(o, i, d):
+#    coor = tf.tile(tf.expand_dims(
+#        [float(int(i / d)) / d, (i % d) / d], axis=0), [BATCH_SIZE, 1])
+#    o = tf.concat([o, tf.to_float(coor)], axis=1)
+#    return o
+#        
+#def g_theta(o_i, o_j, q, scope='g_theta', reuse=True):
+#    with tf.variable_scope(scope, reuse=reuse) as scope:
+##        if not reuse: log.warn(scope.name)
+#        g_1 = fc(tf.concat([o_i, o_j, q], axis=1), 256, name='g_1')
+#        g_2 = fc(g_1, 256, name='g_2')
+#        g_3 = fc(g_2, 256, name='g_3')
+#        g_4 = fc(g_3, 256, name='g_4')
+#        return g_4
+#
+## Classifier: takes images as input and outputs class label [B, m]
+#def CONV(img, q, scope='CONV', is_train):
+#    with tf.variable_scope(scope) as scope:
+##        log.warn(scope.name)
+#        conv_1 = conv2d(img, conv_info[0], is_train, s_h=3, s_w=3, name='conv_1')
+#        conv_2 = conv2d(conv_1, conv_info[1], is_train, s_h=3, s_w=3, name='conv_2')
+#        conv_3 = conv2d(conv_2, conv_info[2], is_train, name='conv_3')
+#        conv_4 = conv2d(conv_3, conv_info[3], is_train, name='conv_4')
+#
+#        # eq.1 in the paper
+#        # g_theta = (o_i, o_j, q)
+#        # conv_4 [B, d, d, k]
+#        d = conv_4.get_shape().as_list()[1]
+#        all_g = []
+#        for i in range(d*d):
+#            o_i = conv_4[:, int(i / d), int(i % d), :]
+#            o_i = concat_coor(o_i, i, d)
+#            for j in range(d*d):
+#                o_j = conv_4[:, int(j / d), int(j % d), :]
+#                o_j = concat_coor(o_j, j, d)
+#                if i == 0 and j == 0:
+#                    g_i_j = g_theta(o_i, o_j, q, reuse=False)
+#                else:
+#                    g_i_j = g_theta(o_i, o_j, q, reuse=True)
+#                all_g.append(g_i_j)
+#
+#        all_g = tf.stack(all_g, axis=0)
+#        all_g = tf.reduce_mean(all_g, axis=0, name='all_g')
+#        return all_g
+#
+#def f_phi(g, scope='f_phi', is_train):
+#    with tf.variable_scope(scope) as scope:
+##        log.warn(scope.name)
+#        fc_1 = fc(g, 256, name='fc_1')
+#        fc_2 = fc(fc_1, 256, name='fc_2')
+#        fc_2 = slim.dropout(fc_2, keep_prob=0.5, is_training=is_train, scope='fc_3/')
+#        fc_3 = fc(fc_2, ANSWER_DICT_LENGTH, activation_fn=None, name='fc_3')
+#        return fc_3
+def concat_coor(o, i, d, batch_size):
+    coor = tf.tile(tf.expand_dims(
+        [float(int(i / d)) / d, (i % d) / d], axis=0), [batch_size, 1])
+    o = tf.concat([o, tf.to_float(coor)], axis=1)
+    return o
+        
+def g_theta(o_i, o_j, q, scope='g_theta', reuse=True):
+    with tf.variable_scope(scope, reuse=reuse) as scope:
+#        if not reuse: log.warn(scope.name)
+        g_1 = fc(tf.concat([o_i, o_j, q], axis=1), 256, name='g_1')
+        g_2 = fc(g_1, 256, name='g_2')
+        g_3 = fc(g_2, 256, name='g_3')
+        g_4 = fc(g_3, 256, name='g_4')
+        return g_4
+
+# Classifier: takes images as input and outputs class label [B, m]
+def CONV(img, q, batch_size, is_train, scope='CONV'):
+    with tf.variable_scope(scope) as scope:
+#        log.warn(scope.name)
+        conv_1 = conv2d(img, conv_info[0], is_train, s_h=3, s_w=3, name='conv_1')
+        conv_2 = conv2d(conv_1, conv_info[1], is_train, s_h=3, s_w=3, name='conv_2')
+        conv_3 = conv2d(conv_2, conv_info[2], is_train, name='conv_3')
+        conv_4 = conv2d(conv_3, conv_info[3], is_train, name='conv_4')
+        
+        eight_neighbor = [(-1, -1),(-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        # eq.1 in the paper
+        # g_theta = (o_i, o_j, q)
+        # conv_4 [B, d, d, k]
+        print (conv_4.shape, 'conv_4.shape')
+        d = conv_4.get_shape().as_list()[1]
+        all_g = []
+        for i in range(d*d):
+            i_x = int(i / d)
+            i_y = int(i % d)
+            o_i = conv_4[:, i_x, i_y, :]
+            o_i = concat_coor(o_i, i, d, batch_size)
+            for x, y in eight_neighbor:
+              j_x = i_x + x
+              j_y = i_y + y
+              j = j_x * d + j_y
+              if j_x >= 0 and j_x <= d-1 and j_y >= 0 and j_y <= d-1:
+                  o_j = conv_4[:, j_x, j_y, :]
+                  o_j = concat_coor(o_j, j, d, batch_size)
+                  if i == 0 and j == 1:
+                      g_i_j = g_theta(o_i, o_j, q, reuse=False)
+                  else:
+                      g_i_j = g_theta(o_i, o_j, q, reuse=True)
+                  all_g.append(g_i_j)
+
+        all_g = tf.stack(all_g, axis=0)
+        all_g = tf.reduce_mean(all_g, axis=0, name='all_g')
+        return all_g
+
+def f_phi(g, is_train, scope='f_phi'):
+    with tf.variable_scope(scope) as scope:
+#        log.warn(scope.name)
+        fc_1 = fc(g, 256, name='fc_1')
+        fc_2 = fc(fc_1, 256, name='fc_2')
+        fc_2 = slim.dropout(fc_2, keep_prob=0.5, is_training=is_train, scope='fc_3/')
+        fc_3 = fc(fc_2, ANSWER_DICT_LENGTH, activation_fn=None, name='fc_3')
+        return fc_3
+
+class LSTM:
+    """"""
+    def __init__(self, layer_size, batch_size):
+        self.hidden_size = layer_size
+        self.batch_size = batch_size
+        self.reuse = False
+    
+    def __call__(self, images, timestep_size):
+        """
+        Args:
+            images: unstacked and stack again, [MX_LEN, batch_size, EMBEDDING_DIM]
+        Returns:
+            outputs: Returns the LSTM outputs, as well as the forward and backward hidden states, 
+                    [MX_LEN, batch_size, hidden_size]
+        """
+        self.timestep_size = timestep_size
+        lstm_inputs = images
+        with tf.variable_scope('lstm'):
+            outputs = []
+            cell = tf.contrib.rnn.BasicLSTMCell(self.hidden_size, forget_bias = 1.0)
+            state = cell.zero_state(self.batch_size, dtype = tf.float32)
+            for timestep in range(self.timestep_size):
+                if timestep == 0:
+                    reuse = self.reuse
+                else:   reuse = True
+#                print (timestep, 'timestep')
+                cell = tf.contrib.rnn.BasicLSTMCell(self.hidden_size, forget_bias = 1.0, reuse = reuse)
+                input_tmp = lstm_inputs[timestep]
+                (cell_output, state) = cell(input_tmp, state) # [batch_size, EMBEDDING_DIM]
+                outputs.append(cell_output)
+        outputs = tf.stack(outputs)
+        print (outputs.shape, 'lstm_outputs.shape')
+        self.reuse = True
+        return outputs
+
+class sentence_embedding:
+    def __init__(self, batch_size):
+        self.reuse = False
+        self.batch_size = batch_size
+        self.dict_length = QUESTION_DICT_LENGTH
+    
+    def __call__(self, sentences):
+        """Embed the sentences, answers with EMBEDDING_DIM
+        Args:
+            sentences:[batch_size, MX_LEN]
+        Returns:
+            embedding: [batch_size, MX_LEN, EMBEDDING_DIM]
+        """
+        with tf.variable_scope('embedding', reuse = self.reuse):
+            clevr_on_GLOVE = np.load('clevr_question_on_GLOVE.npy')
+            init = tf.constant_initializer(clevr_on_GLOVE)
+            embedding_matrix = tf.get_variable('embedding_matrix', shape = [self.dict_length + 1, EMBEDDING_DIM], dtype = tf.float32, 
+                                               initializer = init)
+            print (sentences.shape, 'sentences.shape')
+            onehot_sentences = tf.one_hot(sentences, self.dict_length + 1) # [batch_size, MX_LEN, dict_length]
+            print (onehot_sentences.shape, 'onehot_sentences.shape')
+            # Embed sentences with embedding_matrix
+            embedding = []
+            for b in range(self.batch_size):
+                embedding_tmp = tf.matmul(onehot_sentences[b], embedding_matrix)
+                embedding.append(embedding_tmp)
+            embedding = tf.stack(embedding) # [batch_size, MX_LEN, EMBEDDING_DIM]
+        self.reuse = True
+        
+        return embedding, embedding_matrix
+
+def inference_demo(images, questions, answers, batch_size):
+    
+    lstm = LSTM(layer_size = 128, batch_size = batch_size)
+    embedding_function = sentence_embedding(batch_size = batch_size)
+    
+    embedding, embedding_matrix = embedding_function(questions)
+    timestep_size = questions.shape[1]
+    encoded_sentences = []
+    for sentence in tf.unstack(embedding, axis = 1):
+        encoded_sentences.append(sentence)
+    lstm_encode = lstm(encoded_sentences, timestep_size = timestep_size)[-1]
+    
+    g = CONV(images, lstm_encode, batch_size = batch_size, is_train = True, scope='CONV')
+    logits = f_phi(g, is_train = True, scope='f_phi')
+    all_preds = tf.nn.softmax(logits)
+    print (all_preds.shape, 'all_preds.shape')
+    predict_labels = tf.argmax(all_preds, axis = 1)
+    print (predict_labels.shape, 'predict_labels.shape')
+    
+    return logits, predict_labels
+
+def accuracy(logits, answers):
+    """Calculate accuracy
+    Args:
+        logits: [batch_size, ANSWER_DICT_LENGTH]
+        answers: correct labels [batch_size], each with a number, indicating the index of word in dict
+    Returns:
+        accuracy
+    """
+    # Get the predicted labels, [batch_size]
+    with tf.variable_scope('accuracy'):
+        predict_labels = tf.argmax(tf.nn.softmax(logits, dim = -1), axis = 1)
+#        tf.summary.text('predictions', predict_labels)
+#        tf.summary.text('answers', answers)
+#        tf.summary.text('softmax', tf.nn.softmax(logits, dim = -1))
+    
+    correct_predictions = tf.equal(predict_labels, tf.cast(answers, tf.int64))
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+    
+    tf.summary.scalar('accuracy', accuracy)
+    
+    return accuracy
+
+
+def loss(logits, answers):
+    """Add L2loss to all trainable variables
+    """
+    # Embed answers with embedding_matrix
+    onehot_answers = tf.one_hot(answers, ANSWER_DICT_LENGTH) # [batch_size, answer_dict_length]
+    
+    print (logits.shape, 'logits.shape')
+
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = onehot_answers, name = 'cross_entropy_per_example')
+#    cross_entropy = tf.losses.softmax_cross_entropy(onehot_labels = answers, logits = logits, name = 'cross_entropy_per_example')
+#    mse = tf.div(tf.reduce_mean(tf.square(tf.subtract(logits, embedding))), 2, name ="mse")
+#    tf.add_to_collection('losses', mse)
+    cross_entropy_mean = tf.reduce_mean(cross_entropy, name = 'cross_entropy')
+    tf.add_to_collection('losses', cross_entropy_mean)
+    
+    return tf.add_n(tf.get_collection('losses'), name = 'total_loss')
+    
+
+def _add_loss_summaries(total_loss):
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+    losses = tf.get_collection('losses')
+    loss_averages_op = loss_averages.apply(losses + [total_loss])
+    # Attach a scalar summary to all individual losses and the total loss; do the
+    # same for the averaged version of the losses.
+    for l in losses + [total_loss]:
+    # Name each loss as '(raw)' and name the moving average version of the loss
+    # as the original loss name.
+        tf.summary.scalar(l.op.name +' (raw)', l)
+        tf.summary.scalar(l.op.name, loss_averages.average(l))
+    
+    return loss_averages_op
+
+def train(total_loss, global_step):
+    """Train CIFAR-10 model.
+    Create an optimizer and apply to all trainable variables. Add moving
+    average for all trainable variables.
+    """
+    decay_steps = 20000
+#    
+    lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
+                                    global_step,
+                                    decay_steps,
+                                    LEARNING_RATE_DECAY_FACTOR,
+                                    staircase = True)
+    tf.summary.scalar('learning_rate', lr)
+    
+    loss_average_op = _add_loss_summaries(total_loss)
+    
+    # Compute gradients
+    with tf.control_dependencies([loss_average_op]):
+        #opt = tf.train.GradientDescentOptimizer(lr)
+        opt = tf.train.AdamOptimizer(learning_rate = lr)
+    grads = opt.compute_gradients(total_loss)
+    # Apply gradients
+    apply_gradient_op = opt.apply_gradients(grads, global_step = global_step)
+    
+    for var in tf.trainable_variables():
+        tf.summary.histogram(var.op.name, var)
+    
+    for grad, var in grads:
+        if grad is not None:
+            tf.summary.histogram(var.op.name + '/gradients', grad)
+    
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
+    
+    for item in tf.trainable_variables():
+        print (item.name)
+    
+    with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
+      train_op = tf.no_op(name='train')
+    return train_op
+    
+    
+    
---
+++ try_path/train.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15:41:24 2017
+
+@author: xifan
+"""
+
+import tensorflow as tf
+import numpy as np
+import model
+import os
+from six.moves import xrange
+import time
+from datetime import datetime
+from tensorflow.python.platform import gfile
+import json
+from scipy.misc import imshow, imsave
+
+#import sys
+#sys.path.append(['/usr/lib/python2.7.zip', '/usr/lib/python2.7', '/usr/lib/python2.7/plat-linux2', '/usr/lib/python2.7/lib-tk', '/usr/lib/python2.7/lib-old', '/usr/lib/python2.7/lib-dynload', '/usr/lib/python2.7/sie'])
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('train_dir', 'train_rev', """Directory where to write event logs and checkpoint.""")
+tf.app.flags.DEFINE_integer('max_steps', 1000000, """Number of batches to run.""")
+tf.app.flags.DEFINE_integer('batch_size', 16, """Number of examples per batch""")
+tf.app.flags.DEFINE_boolean('log_device_placement', False, """Whether to log device placement.""")
+
+channels = 3
+
+EMBEDDING_DIM = 50
+MX_LEN = 64
+
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+QUESTION_DICT_LENGTH = 80
+ANSWER_DICT_LENGTH = 28
+
+train_json = '../nlvr-master/train/train.json'
+train_img_folder = '../nlvr-master/train/images'
+test_json = '../nlvr-master/dev/dev.json'
+test_img_folder = '../nlvr-master/dev/images'
+
+sentence_log = 'sentence_log.txt'
+
+# Load dicts
+f = open('my_question_index_to_word.json', 'r')
+question_index_to_word = json.load(f)
+f.close()
+f = open('my_question_word_to_index.json', 'r')
+question_word_to_index = json.load(f)
+f.close()
+QUESTION_DICT_LENGTH = len(question_word_to_index)
+f = open('my_answer_index_to_word.json', 'r')
+answer_index_to_word = json.load(f)
+f.close()
+f = open('my_answer_word_to_index.json', 'r')
+answer_word_to_index = json.load(f)
+f.close()
+ANSWER_DICT_LENGTH = len(answer_word_to_index)
+
+def read_and_decode(filename):
+    # Build a filename_queue according to filename
+    filename_queue = tf.train.string_input_producer([filename])
+
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'image' : tf.FixedLenFeature([], tf.string),
+                                           'question': tf.FixedLenFeature([], tf.string),
+                                           'answer': tf.FixedLenFeature([], tf.int64),
+                                       })
+
+    image = tf.decode_raw(features['image'], tf.uint8)
+    image = tf.reshape(image, [320, 480, 3])
+    image = tf.cast(image, tf.float32) * (1. / 255) - 0.5
+    
+    sentence = tf.decode_raw(features['question'], tf.int64)
+#    sentence = tf.reshape(features['question'], [MX_LEN])
+    sentence = tf.reshape(sentence, [MX_LEN])
+    sentence = sentence[:50]
+    sentence = tf.cast(sentence, tf.int32)
+
+    answer = tf.cast(features['answer'], tf.int32)
+
+    return image, sentence, answer
+
+def generate_batch(batch_size, flag):
+#    image, sentence, answer = read_and_decode("/home/zhangxifan/tasks_emb/train_emb.tfrecords")
+#    image, sentence, answer = read_and_decode("/home/mi/RelationalReasoning/RelationalReasoning/tasks_emsb/train_emb.tfrecords")
+    image, sentence, answer = read_and_decode('/home/zhangxifan/train_balance.tfrecords')
+#    image, sentence, answer = read_and_decode('/home/RelationalReasoning/train_balance.tfrecords')
+
+    min_fraction_of_example_in_queue = 0.4
+    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * min_fraction_of_example_in_queue)
+    num_preprocess_threads = 5
+    
+    images, sentences_batch, answers_batch = tf.train.shuffle_batch([image, sentence, answer],
+                                                 batch_size = batch_size,
+                                                 num_threads = num_preprocess_threads,
+                                                 capacity = min_queue_examples + 3 * batch_size,
+                                                 min_after_dequeue = min_queue_examples)
+#    answers_batch = tf.squeeze(answers_batch, axis = 1)
+    return images, sentences_batch, answers_batch
+
+def main(argv = None):
+    
+    if gfile.Exists(FLAGS.train_dir):
+        gfile.DeleteRecursively(FLAGS.train_dir)
+        gfile.MakeDirs(FLAGS.train_dir)
+
+    with tf.Graph().as_default():
+        global_step = tf.Variable(0, trainable = False)
+        # Get images, sentences, labels batch for RN
+        images, sentences, answers = generate_batch(batch_size=FLAGS.batch_size, flag='train')
+        images = tf.image.resize_bilinear(images, size = [240, 240])
+        tf.summary.image('image', tf.expand_dims(images[0], axis = 0))
+        print (images)
+        print (sentences)
+        print (answers)
+        
+        # Build a graph that computer the logits predictions from inference model
+#        logits, cnn_features = model.inference(images, sentences, answers)
+        logits, predict_labels = model.inference_demo(images, sentences, answers, FLAGS.batch_size)
+        # Calculate loss
+        loss = model.loss(logits, answers)
+        
+        # gradCAM
+#        softmax_linear = tf.nn.softmax(logits, dim = -1)[0, :]
+##        print (softmax_linear)
+##        print (softmax_linear[answers[0]])
+##        print (loss)
+#        grads = tf.gradients(softmax_linear[answers[0]], cnn_features)
+#        print (grads, 'grads')
+##        grads_oneimage = tf.expand_dims(grads[0], 0) # summary_image request [batch_size, height, width, nchannels]
+#        grads_oneimage = tf.maximum(grads[0][0], 0)
+#        grads_max = tf.reduce_max(grads_oneimage)
+#        importance_weights = tf.reduce_mean(tf.reduce_mean(grads_oneimage, 0, keep_dims=True), 1, keep_dims=True)
+#        print (importance_weights.shape, 'importance_weights.shape')
+#        grads_oneimage = tf.multiply(importance_weights, grads_oneimage)
+#        grads_oneimage = tf.reduce_mean(grads_oneimage, 2, keep_dims = True) / grads_max * 255
+#        print (grads_oneimage, 'grads_oneimage')
+#        grads = tf.gradients(softmax_linear[answers[0]], images)
+#        backpro_oneimage = tf.maximum(grads[0][0], 0)
+#        backpro_oneimage = tf.reduce_mean(backpro_oneimage, 2, keep_dims=True) / tf.reduce_max(backpro_oneimage) * 255
+#        grads_oneimage = tf.squeeze(tf.image.resize_bilinear(tf.expand_dims(grads_oneimage, 0), size = [320, 480]), axis = 0)
+        
+#        grad_cam = tf.multiply(grads_oneimage, backpro_oneimage)
+#        tf.summary.image('grad-cam', tf.expand_dims(grad_cam, 0))
+
+#        acc = model.accuracy(logits, answers, embedding_matrix)
+
+#        acc = model.accuracy(logits, answers)
+        acc = tf.reduce_mean(tf.cast(tf.equal(predict_labels, tf.cast(answers, tf.int64)), tf.float32))
+        tf.summary.scalar('accuracy', acc)
+        # Build a graph that trains the model with one batch of examples and updates the model parameters
+        train_op = model.train(loss, global_step)
+        # Create a Saver
+        saver = tf.train.Saver(tf.global_variables())
+        # Build the summary operation based on the TF collection of Summaries
+        summary_op = tf.summary.merge_all()
+        # Build an initialization operation to run below.
+        init = tf.global_variables_initializer()
+        # Start running operations on the Graph.
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7, allow_growth = True)
+        sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False, gpu_options = gpu_options))
+        sess.run(init)
+        ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            # Restores from checkpoint
+            print ('Restore from' + ckpt.model_checkpoint_path)
+            saver.restore(sess, ckpt.model_checkpoint_path)
+
+        tf.train.start_queue_runners(sess = sess)
+        summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
+        
+        start_time = time.time()
+        
+        average_loss = 0
+        for step_tmp in xrange(FLAGS.max_steps):
+            
+            step = step_tmp + 0
+            
+            start_time = time.time()
+            
+#            s = sess.run([sentences])
+#            print (s)
+#            _, loss_value, accuracy_value = sess.run([train_op, loss, acc])
+            answers_value, _, loss_value, accuracy_value, predict_labels_value = sess.run([answers, train_op, loss, acc, predict_labels])
+            
+#            print (logits_value[0])
+#            print (softmax_linear_value[0])
+            # test dataset
+#            for b in range(FLAGS.batch_size):
+#                imsave('pic/' + str(step) + '_' + str(b) + '.bmp', images_value[b])
+#                for i in range(MX_LEN):
+#    #                    f.write(str(sentences_value[0, i]) + ' ')
+#                    if sentences_value[b, i] != 0 :
+#                        print question_index_to_word[str(sentences_value[b, i])], # python2
+#                print ('\n')
+#                print (answer_index_to_word[str(answers_value[b])])
+##                print ('\n!')
+
+#            predict_labels, accuracy_value = model.accuracy(logits_value, answers, embedding_matrix_value)
+            duration = time.time() - start_time
+            
+            for i in range(FLAGS.batch_size):
+                # display
+                prediction_str = str(predict_labels_value[i])
+                label_str = str(answers_value[i])
+                print (answer_index_to_word[prediction_str], answer_index_to_word[label_str])
+
+            average_loss += loss_value
+            
+            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+            if step % 1 == 0:
+#                num_examples_per_step = FLAGS.batch_size
+                examples_per_sec = FLAGS.batch_size / duration
+#                sec_per_batch = float(duration)
+                
+                format_str = ('%s: step %d, %.2f, loss = %.5f, average_loss = %.5f, accuracy = %.5f')
+                print (format_str % (datetime.now(), step, examples_per_sec, loss_value, average_loss/(step + 1), accuracy_value))
+#                format_str = ('%s: step %d, loss = %d')
+#                print (format_str % (datetime.now(), step, loss_value))
+            
+            if step % 100 == 0:
+                # print sentences to file
+#                f = open(sentence_log, 'a')
+#                f.write('step: ' + str(step) + ' ')
+#                for i in range(MX_LEN):
+##                    f.write(str(sentences_value[0, i]) + ' ')
+#                    if sentences_value[0, i] != 0 :
+#                        f.write(question_index_to_word[str(sentences_value[0, i])] + ' ')
+#                f.write(answer_index_to_word[str(answers_value[0])])
+#                imsave('pic/' + str(step) + '.bmp', images_value[0])
+#                f.write('\n')
+#                f.close()
+                
+                summary_str = sess.run(summary_op)
+                summary_writer.add_summary(summary_str, step)
+            
+            if step % 2000 == 0:
+                checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
+    
+    
+if __name__ == '__main__':
+    tf.app.run()
+    
